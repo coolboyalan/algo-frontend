aa96742fce5361dc32a952738bef1f489969b439
Alan <alan@macbook-air.local>
2025-11-12 00:24:58 +0530

    fixed theme provider. Bugs still pending

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { type ThemeProviderProps } from 'next-themes';
 import { useSettingsStore } from '@/store/settings-store';
 import { useTheme } from 'next-themes';
 
